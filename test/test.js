5eb6603900f0d56a1aeba12a8f4d44cb03be7505
Dave Longley <dlongley@digitalbazaar.com>
2025-03-31 17:13:23 -0400

    Add custom targeted `audience` / oauth2 route namespace feature.

--- test/test.js
+++ test/test.js
@@ -10,6 +10,25 @@ import '@bedrock/express';
 
 // add OAuth2 authz server routes
 bedrock.events.on('bedrock-express.configure.routes', app => {
+  const {baseUri} = bedrock.config.server;
+
+  const additionalClients = new Map([
+    ['5f4e027b-efb1-4bf4-b741-69d16338e47e', {
+      id: '5f4e027b-efb1-4bf4-b741-69d16338e47e',
+      allowedScopes: ['read:/test-authorize-request'],
+      secretHash: 'wkueZ4zwWnw6J1xV3jtEfSqnE7yJutgcWL-sQb7OnZ8'
+    }],
+    // namespaced client, i.e., special audience
+    ['ee8f6e61-a6c7-4ada-846e-9cbca3b15661', {
+      id: 'ee8f6e61-a6c7-4ada-846e-9cbca3b15661',
+      allowedScopes: [
+        'read:/test-authorize-request',
+      ],
+      audience: `${baseUri}/namespace`,
+      secretHash: 'Fikr8BzsZB5f0mEGeRTcIVqLxpgSCpTwkYOtyaeHNTs'
+    }]
+  ]);
+
   addOAuth2AuthzServer({
     app,
     async getOAuth2Client({clientId} = {}) {
@@ -20,11 +39,7 @@ bedrock.events.on('bedrock-express.configure.routes', app => {
       } catch(e) {}
 
       // now return client w/custom storage implementation
-      const client = {
-        id: '5f4e027b-efb1-4bf4-b741-69d16338e47e',
-        allowedScopes: ['read:/test-authorize-request'],
-        secretHash: 'wkueZ4zwWnw6J1xV3jtEfSqnE7yJutgcWL-sQb7OnZ8'
-      };
+      const client = additionalClients.get(clientId);
       return client;
     }
   });
@@ -38,6 +53,13 @@ bedrock.events.on('bedrock-express.configure.routes', app => {
   app.get(
     '/test-authorize-request',
     middleware.authorizeRequest(), (req, res) => {
+      res.json({success: true});
+    });
+  app.get(
+    '/namespace/test-authorize-request',
+    middleware.authorizeRequest({
+      rootInvocationTarget: `${baseUri}/namespace`
+    }), (req, res) => {
       res.json({success: true});
     });
 });
